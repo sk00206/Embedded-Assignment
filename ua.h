dd61458d8bd12ccdb31462452e18471502096619
sk00206 <99183661+sk00206@users.noreply.github.com>
2022-11-03 15:01:37 +0200

    Add files via upload

---
+++ ua.h
@@ -0,0 +1,16 @@
+#ifndef __UART__
+#define __UART__
+
+void Uart_Init(void);
+
+void Uart_SetBaudRate(unsigned short BuadRate);
+
+void Uart_SendChar(unsigned char DataByte);
+
+unsigned char Uart_IsDataAvailable(void);
+
+unsigned char Uart_ReadData();
+
+void Uart_SendString(unsigned char DataString[], unsigned char Size);
+
+#endif
