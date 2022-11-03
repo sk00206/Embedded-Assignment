385198e298614dc5e107cb5473ac25883a991eb4
sk00206 <99183661+sk00206@users.noreply.github.com>
2022-11-03 15:03:37 +0200

    Add files via upload

---
+++ dio.h
@@ -0,0 +1,4 @@
+Dio_Init()
+Dio_SetPinDirection(unsigned char Port, unsigned char Pin, unsigned char Direction);
+Dio_SetPinState(unsigned char Port,unsigned char Pin, unsigned char State);
+unsigned char Dio_GetPinState(unsigned char Port,unsigned char Pin);
